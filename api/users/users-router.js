ddcfdddd7313d11cd5fa3d08b0dbfebee8510b67
Bobby Gomez <bobizzle@Bobbys-MacBook-Pro.local>
2022-01-12 20:25:02 -0600

    having issues with post/api/users

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,36 +1,82 @@
 const express = require('express');
 const { 
   validateUserId,
+  validateUser,
+  validatePost,
 } = require('../middleware/middleware')
+
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
-})
+router.get('/', (req, res, next) => {
+  User.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
+});
 
 router.get('/:id', validateUserId, (req, res) => {
-  
-})
-
-router.post('/', (req, res) => {
-  
-})
-
-router.put('/:id', validateUserId, (req, res) => {
-  
-})
-
-router.delete('/:id', validateUserId, (req, res) => {
-  
-})
-
-router.get('/:id/posts', validateUserId, (req, res) => {
-  
-})
-
-router.post('/:id/posts', validateUserId, (req, res) => {
-
+  res.json(req.user)
 });
+
+router.post('/', validateUserId, (req, res, next) => {
+  User.insert({ name: req.name })
+  .then(newUser => {
+    res.status(201).json(newUser)
+  })
+  .catch(next)
+});
+
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  User.update(req.params.id, { name: req.name })
+  .then(() => {
+    return User.getById(req.params.id)
+  })
+  .then(user => {
+    res.json(user)
+  })
+  .catch(next)
+});
+
+router.delete('/:id', validateUserId, async (req, res, next) => {
+  try {
+    await User.remove(req.params.id)
+    res.json(req.user)
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
+  try {
+    const result = await User.getUserPosts(req.params.id)
+    res.json(result)
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
+  try {
+    const result = await Post.insert({
+      user_id: req.params.id,
+      text: req.text,
+    })
+    res.status(201).json(result)
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      customMessage:'something bad happened inside posts router', 
+      message: err.message,
+      stack: err.stack,
+    })
+})
 
 module.exports = router
